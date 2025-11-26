5dc6bb77034eb936c17a38752cbc5e746143928a
Estudiante <60031750@idexperujapon.edu.pe>
2025-11-26 16:26:40 -0500

    cree el formulario equipos

--- INVENTARIO_EQUIPOSINFORMATICOS/PrincipalFrm.cs
+++ INVENTARIO_EQUIPOSINFORMATICOS/PrincipalFrm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace INVENTARIO_EQUIPOSINFORMATICOS
 {
-    public partial class Equipos : Form
+    public partial class PrincipalFrm : Form
     {
-        public Equipos()
+        public PrincipalFrm()
         {
             InitializeComponent();
         }
